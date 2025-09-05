58f52e4d80006181630b1e8e794d8e3861697e60
DaviMendesJ <mdavifilho786@gmail.com>
2025-09-05 15:09:21 -0300

    Atividade 1 JavaScript Contagem

---
+++ Exercicio01.js
@@ -0,0 +1,8 @@
+// 1) Crie um programa que faça uma contagem de 1 a 10
+var contagems = 11;
+
+for(let i = 0; i < contagems; i++){
+    let contas_nova = console.log('Numero: ', i)
+    //console.log(contas_nova)    
+    }
+; 
